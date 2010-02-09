60cbbaf97890fabb65af7d53d09739eed59b6cb9
Arne Blankerts <Arne@Blankerts.de>
2010-02-10 00:01:57 +0100

    Added more replacements for pear package building ./. development structure

--- pear.sh
+++ pear.sh
@@ -5,7 +5,9 @@ cp -r src/* tmp/TheSeer/Autoload
 cp package.xml tmp
 cp phpab.bat tmp
 cd tmp
-sed -e "s@require __DIR__ . '/src/@require 'TheSeer/Autoload/@" ../phpab.php > phpab.php
+sed -e "s@require __DIR__ . '/src/@require 'TheSeer/Autoload/@" ../phpab.php | \
+ sed -e "s@require __DIR__ . '/../ezc/current/Base/src/@require 'ezc/Base/@" | \
+ sed -e "s@require __DIR__ . '/../DirectoryScanner/@require 'TheSeer/DirectoryScanner/@" > phpab.php
 pear package
 mv Autoload*.tgz ..
 cd ..
