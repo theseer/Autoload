e31bf6d74d7a84fa4029b650cc541fdc7c5bf85e
Arne Blankerts <Arne@Blankerts.de>
2010-03-30 15:59:18 +0200

    Added ___AUTOLOAD___ variable for uniqid based function name to support php 5.2 without lamda function

--- src/autoloadbuilder.php
+++ src/autoloadbuilder.php
@@ -228,9 +228,9 @@ namespace TheSeer\Tools {
          $replace = array_merge($this->variables, array(
             '___CREATED___'   => date( $this->dateformat, $this->timestamp ? $this->timestamp : time()),
             '___CLASSLIST___' => join( ',' . $this->linebreak . $this->indent, $entries),
-            '___BASEDIR___'   => $this->baseDir ? '__DIR__ . ' : ''
+            '___BASEDIR___'   => $this->baseDir ? '__DIR__ . ' : '',
+            '___AUTOLOAD___'  => uniqid('autoload')
          ));
-
          return str_replace(array_keys($replace), array_values($replace), $this->template);
       }
 
