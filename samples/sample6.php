968888f8b88c4317dbeb6895a34c944bea5b859c
Arne Blankerts <Arne@Blankerts.de>
2010-10-17 14:58:34 +0200

    adjusted sample code for depdendency sorting

--- samples/sample6.php
+++ samples/sample6.php
@@ -3,11 +3,13 @@
 require '../src/classfinder.php';
 require '../src/dependencysorter.php';
 
-$finder = new \TheSeer\Tools\ClassFinder;
-$found = $finder->parseFile('../tests/_data/dependency/file2.php');
-$found = array_merge($found, $finder->parseFile('../tests/_data/dependency/file1.php'));
+$finder = new \TheSeer\Tools\ClassFinder(true);
+$finder->parseFile('../tests/_data/dependency/file2.php');
+$finder->parseFile('../tests/_data/dependency/file1.php');
+
+$found = $finder->getClasses();
 
 
-$x = new \TheSeer\Tools\classDependecySorter($found, $finder->getDependencies());
+$x = new \TheSeer\Tools\classDependencySorter($found, $finder->getDependencies());
 $r = $x->process();
 var_dump($found, $finder->getDependencies(), $r);
