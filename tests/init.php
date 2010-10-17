daa4e5ccfb438ff3ed240d1828a58ff60df0601a
Arne Blankerts <Arne@Blankerts.de>
2010-10-17 14:57:58 +0200

    load dependency sorting class

--- tests/init.php
+++ tests/init.php
@@ -3,3 +3,4 @@
 require __DIR__ . '/../src/classfinder.php';
 require __DIR__ . '/../src/phpfilter.php';
 require __DIR__ . '/../src/autoloadbuilder.php';
+require __DIR__ . '/../src/dependencysorter.php';
