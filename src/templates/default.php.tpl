theseer/Autoload

commit e2ca054d8f0edf5ce827c54fd2a7c9bd39d1fd7e

Changed static variable assignment to work around a possible performance bug in php

src/templates/default.php.tpl
@@ -1,8 +1,10 @@
 <?php // this is an autogenerated file - do not edit (created ___CREATED___)
 spl_autoload_register(
    function($class) {
-      static $classes = array(
-         ___CLASSLIST___
+      static $classes = null;
+      if ($classes === null) {
+         $classes = array(
+            ___CLASSLIST___
       );
       $cn = strtolower($class);
       if (isset($classes[$cn])) {

src/templates/phar.php.tpl
@@ -1,8 +1,10 @@
 <?php
 spl_autoload_register(
    function($class) {
-      static $classes = array(
-         ___CLASSLIST___
+      static $classes = null;
+      if ($classes === null) {
+         $classes = array(
+            ___CLASSLIST___
       );
       $cn = strtolower($class);
       if (isset($classes[$cn])) {

src/templates/php52.php.tpl
@@ -1,7 +1,9 @@
 <?php // this is an autogenerated file - do not edit (created ___CREATED___)
 function ___AUTOLOAD___($class) {
-   static $classes = array(
-         ___CLASSLIST___
+      static $classes = null;
+      if ($classes === null) {
+         $classes = array(
+            ___CLASSLIST___
    );
    $cn = strtolower($class);
    if (isset($classes[$cn])) {

commit 3346edd472648085498ec21d38da75750eaa4650

fixed a critical typo in several templates
was introduced in e2ca054d8f0edf5ce827c54fd2a7c9bd39d1fd7e

src/templates/default.php.tpl
@@ -5,7 +5,8 @@ spl_autoload_register(
       if ($classes === null) {
          $classes = array(
             ___CLASSLIST___
-      );
+          );
+      }
       $cn = strtolower($class);
       if (isset($classes[$cn])) {
          require ___BASEDIR___$classes[$cn];

src/templates/phar.php.tpl
@@ -5,7 +5,8 @@ spl_autoload_register(
       if ($classes === null) {
          $classes = array(
             ___CLASSLIST___
-      );
+         );
+      }
       $cn = strtolower($class);
       if (isset($classes[$cn])) {
          require 'phar://___PHAR___' . $classes[$cn];

src/templates/php52.php.tpl
@@ -4,7 +4,8 @@ function ___AUTOLOAD___($class) {
       if ($classes === null) {
          $classes = array(
             ___CLASSLIST___
-   );
+         );
+      }
    $cn = strtolower($class);
    if (isset($classes[$cn])) {
       require ___BASEDIR___$classes[$cn];