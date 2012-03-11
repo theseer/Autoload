29a05db5d14293ff82264881c38344954d6c4de5
Arne Blankerts <Arne@Blankerts.de>
2012-03-11 16:31:04 +0100

    'Use' testcases

---
+++ tests/_data/classfinder/use5.php
@@ -0,0 +1,10 @@
+<?php
+
+   class demo2 { }
+   
+   use demo2 as foo;
+
+ $x = new foo();
+ var_dump($x);
+
+?>
---
+++ tests/_data/classfinder/use7.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace demo\a {
+
+   $x = function($y) use ($a) {};
+
+   class demo2 {
+   }
+}
+?>
