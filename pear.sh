0ade5a20f370bed0fadf355ee90c006dbc801385
Arne Blankerts <Arne@Blankerts.de>
2012-10-15 17:54:31 +0200

    Update structure to make Autoload Builder be packagable by Fedora and possibly other distros

--- pear.sh
+++ pear.sh
@@ -2,9 +2,11 @@
 rm -f Autoload*.tgz
 mkdir -p tmp/TheSeer/Autoload
 cp -r src/* tmp/TheSeer/Autoload
-cp package.xml tmp
+cp package.xml phpunit.xml.dist LICENSE README.md tmp
+cp -r tests tmp
 cp phpab.* tmp
 cd tmp
+php ../../DirectoryScanner/samples/pear-package.php ../package.xml . | xmllint --format - > package.xml
 pear package
 mv Autoload*.tgz ..
 cd ..
